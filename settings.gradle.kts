bc7942ecb0fff0d1617a02d8e10225bcf6b94f2f
shivam340 <shivam.gosavi@go-pay.co.id>
2020-03-03 15:16:48 +0530

    [Shivam][GradleLearning1] accessed user defined property in settings file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,3 +10,12 @@ println("$TAG  project name is ${rootProject.name}")
 
 // can add projects which are part of build process.
 include(":app")
+
+
+// access user defined properties using extra
+val greetMessage = extra.properties.get("greetMessage")
+if(greetMessage != null) {
+    println("$TAG  value of user defined property greetMessage is $greetMessage")
+}else{
+    println("$TAG  user defined property greetMessage is not available.")
+}
